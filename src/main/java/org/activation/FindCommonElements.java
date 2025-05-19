9ef23ffc4477c32f240b1ee2cdb6a95a01bc8bee
gauri <gauri@weetechSolution.com>
2025-05-19 10:45:12 +0530

    find Comman Element in the Array

--- src/main/java/org/activation/FindCommonElements.java
+++ src/main/java/org/activation/FindCommonElements.java
@@ -1,4 +1,17 @@
 package org.activation;
 
 public class FindCommonElements {
+
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 3, 4};
+        int[] arr2 = {3, 4, 5, 6};
+
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr2.length ; j++) {
+                if (arr1[i] == arr2[j]) {
+                    System.out.print(arr1[i] + " ");  // 3 4
+                }
+            }
+        }
+    }
 }
