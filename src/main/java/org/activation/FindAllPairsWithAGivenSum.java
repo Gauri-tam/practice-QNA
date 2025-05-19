52b655f37ecd5d973802253cc8df602fb35ecf93
gauri <gauri@weetechSolution.com>
2025-05-19 10:44:19 +0530

    find pair with the given Sum

--- src/main/java/org/activation/FindAllPairsWithAGivenSum.java
+++ src/main/java/org/activation/FindAllPairsWithAGivenSum.java
@@ -1,4 +1,18 @@
 package org.activation;
 
 public class FindAllPairsWithAGivenSum {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 4, 5, 3, 2};
+        int target = 6;
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] + arr[j] == target) {
+                    System.out.println(arr[i] + " + " + arr[j] + " = " + target);  // 3 + 3 = 6 (if duplicate 3s existed)
+                }
+            }
+        }
+    }
 }
