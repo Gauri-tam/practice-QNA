bb38dddbdcbb20fc3b4d021c38daf83088839b46
gauri <gauri@weetechSolution.com>
2025-05-19 10:46:40 +0530

    Remove Dublicates fron the Sorted Array

--- src/main/java/org/activation/RemoveDuplicatesFromSortedArray.java
+++ src/main/java/org/activation/RemoveDuplicatesFromSortedArray.java
@@ -1,4 +1,24 @@
 package org.activation;
 
 public class RemoveDuplicatesFromSortedArray {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 2, 2, 3, 3};
+        int[] temp = new int[arr.length];
+        int j = 0;
+
+        System.out.println(arr.length);
+
+        for (int i = 0; i < arr.length -1; i++) {
+            if (arr[i] != arr[i + 1]) {
+                temp[j++] = arr[i];
+            }
+        }
+
+        temp[j++] = arr[arr.length - 1];  // without this line you only get the 1 2
+
+        for (int i = 0; i < j; i++) {
+           System.out.print(temp[i] + " ");
+        }
+    }
 }
