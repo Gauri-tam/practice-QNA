d26cceecaa339f7e4a6d61b3178d23a90dedb389
gauri <gauri@weetechSolution.com>
2025-05-20 13:48:30 +0530

    Added Palindrome String program

---
+++ src/main/java/org/activation/stringDemo/Palindrome.java
@@ -0,0 +1,19 @@
+package org.activation.stringDemo;
+
+public class Palindrome {
+
+    public static void main(String[] args) {
+        String str = "please";
+        String reversedStr = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversedStr += str.charAt(i);
+        }
+
+        if (str.equals(reversedStr)) {  // dont use == for string comparison
+            System.out.println("the string is a palindrome");
+        } else {
+            System.out.println("the string is not a palindrome");
+        }
+    }
+}
